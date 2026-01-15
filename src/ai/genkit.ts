56e2410fff6735fda8fa87574a7b74b337a83c72
Alexa Josean <andigalexa@gmail.com>
2026-01-15 18:33:25 +0800

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI({clientOptions: {skipRefererCheck: true}})],
-  model: 'googleai/gemini-2.0-flash-lite',
+  model: 'googleai/gemini-3.0',
 });
